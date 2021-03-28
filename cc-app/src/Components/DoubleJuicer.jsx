559493b6c9d6c6e526a3ef8e8acaab6d3f574b55
Nathan Inbar <nathaninbar@gmail.com>
2021-03-28 01:04:26 -0700

    🧃DOUBLE JUICERS POG

--- cc-app/src/Components/DoubleJuicer.jsx
+++ cc-app/src/Components/DoubleJuicer.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import '../Styles/DoubleJuicer.css';
+import '../Styles/Juicer.css';
 
-function DoubleJuicer({metric_icon, metric_name, data}) {
+function DoubleJuicer({metric_icon1, metric_name1, data1, metric_icon2, metric_name2, data2}) {
     
     return (
-        <div className="juicer__container">
-            <div className="juicer__image__container">
-                {metric_icon}
+        <div className="djuicer__container">
+
+            <div>
+                <div className="juicer__image__container">
+                    {metric_icon1}
+                </div>
+
+                <div className="djuicer__text">
+                    <p>{metric_name1}</p>
+                    <p>{data1}</p>
+                </div>
             </div>
 
-            <div className="juicer__text">
-                <p>{metric_name}</p>
-                <p>{data}</p>
+            <div className="divisor_vr"></div>
+
+            <div>
+                <div className="juicer__image__container">
+                    {metric_icon2}
+                </div>
+
+                <div className="djuicer__text">
+                    <p>{metric_name2}</p>
+                    <p>{data2}</p>
+                </div>
             </div>
+
         </div>
     );
 }
